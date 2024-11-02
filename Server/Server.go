a60e28f1ebe2cb4a7174122a13e48542124cd8b4
ProjectCaffeine <smith_eg21@yahoo.com>
2024-11-01 20:15:47 -0500

    added listener

--- Server/Server.go
+++ Server/Server.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	ln, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatal("Failed to open TCP connection")
+	}
+
+	fmt.Print("Opening connection on port 8080")
+	for {
+		conn, err := ln.Accept()
+
+		if err != nil {
+			log.Fatal("Failed to accept connection")
+		}
+
+		conn.Close()
+		break;
+	}
+
+	fmt.Print("Closing connection")
+	ln.Close()
 }
